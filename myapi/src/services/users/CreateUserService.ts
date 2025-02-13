afc116e1a230c06e13654044e4392657b637b5cf
yusley <yusley.santos23@gmail.com>
2025-02-13 16:45:51 -0500

    create user and login

--- myapi/src/services/users/CreateUserService.ts
+++ myapi/src/services/users/CreateUserService.ts
@@ -1,27 +1,44 @@
 import prismaClient from "../../prisma";
 import { z } from 'zod'
-import { Conflict } from "../../middlewares/errors";
+import { BaseError, Conflict } from "../../middlewares/errors";
 import { UserSchema } from "./validUserSchema/UserSchema";
 
 class CreateUserService{
     async execute (user: z.infer<typeof UserSchema> ) {
-        const findUser = await prismaClient.user.findFirst({
-            where: {
-                cpf: user.cpf
+        prismaClient.$transaction(async (tx) => {
+            const findUser = await tx.user.findFirst({
+                where: {
+                    cpf: user.cpf
+                }
+            })
+            if (findUser){
+                throw new Conflict("Usuário já existe")
             }
-        })
-        if (findUser){
-            throw new Conflict("Usuário já existe")
-        }
-        const userCreated = await prismaClient.user.create({
-            data: {
-                name: user.name,
-                cpf: user.cpf,
-                email: user.email,
-                status: user.status
+            const userCreated = await tx.user.create({
+                data: {
+                    name: user.name,
+                    cpf: user.cpf,
+                    email: user.email,
+                    status: user.status
+                }
+            })
+
+            if(!userCreated){
+                throw new BaseError("Erro ao criar usuário", 400)
             }
+            
+            const loginUserCreated = await tx.loginUser.create({
+                data : {
+                    username: user.cpf,
+                    password: 'teste',
+                    userId: userCreated.id
+                }
+            })
+
+            return userCreated
+            
         })
-        return userCreated
+        
     }
 };
 
